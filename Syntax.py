40bd0eefb82d2f082a7c86bdbdda5e47d9889650
AnimeLordx <120332363+AnimeLordx@users.noreply.github.com>
2024-06-03 10:42:16 +0300

    Create Syntax.py

---
+++ Syntax.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+
+#Example1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#Example2
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
